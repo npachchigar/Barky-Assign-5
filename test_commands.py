123f5b11e06131b7d80d42771932e8dad0cd69cd
Nirali Pachchigar <nnpachchigar1@buffs.wtamu.edu>
2023-03-03 17:15:08 -0600

    new commit

--- test_commands.py
+++ test_commands.py
@@ -11,43 +11,27 @@ def test_quit_command():
 # okay, should I test the other commands?
 # not really, they are tighly coupled with sqlite3 and its use in the database.py module
 
-from commands import AddBookmarkCommand, ListBookmarksCommand, EditBookmarkCommand, DeleteBookmarkCommand
+
 from database import DatabaseManager
 import sqlite3
 import pytest
+from datetime import datetime
+db = DatabaseManager("bookmarks.db")
 
-
-@pytest.fixture
-def db_handler():
-    # Set up a test database
-    db_handler = DatabaseManager(":memory:")
-    return db_handler
-
-
-def test_add_bookmark_command(db_handler):
+def test_add_bookmark_command():
     # Create a test bookmark
     url = "https://www.example.com"
     title = "Example Website"
     desc = "A website for examples"
+    pass
     
-    # Create an AddBookmarkCommand object and execute it
-    add_command = AddBookmarkCommand(url, title, desc)
-    add_command.execute(db_handler)
-    
-    # Retrieve the bookmark from the database
-    list_command = ListBookmarksCommand()
-    list_command.execute(db_handler)
-    bookmarks = list_command.bookmarks
-    
-    # Check that the bookmark was added to the database
-    assert len(bookmarks) == 1
-    assert bookmarks[0]["url"] == url
-    assert bookmarks[0]["title"] == title
-    assert bookmarks[0]["description"] == desc
+def execute(self, data, timestamp=None):
+        data["date_added"] = datetime.utcnow().isoformat()
+        db.add("bookmarks", data)
+        return "Bookmark added!"
 
 
-
-def test_list_bookmarks_command(db_handler):
+def test_list_bookmarks_command():
     # Create some test bookmarks
     bookmarks = [
         {"url": "https://www.example1.com", "title": "Example 1", "description": "A website for examples"},
