8e226f05c5e7986fcd750472a967a05d5cabd557
Nirali Pachchigar <nnpachchigar1@buffs.wtamu.edu>
2023-03-06 22:53:20 -0600

    add new commit

--- test_commands.py
+++ test_commands.py
@@ -12,30 +12,4 @@ def test_quit_command():
 # not really, they are tighly coupled with sqlite3 and its use in the database.py module
 
 
-from database import DatabaseManager
-import sqlite3
-import pytest
-from datetime import datetime
-db = DatabaseManager("bookmarks.db")
-
-def test_add_bookmark_command():
-    # Create a test bookmark
-    url = "https://www.example.com"
-    title = "Example Website"
-    desc = "A website for examples"
-    pass
-    
-def execute(self, data, timestamp=None):
-        data["date_added"] = datetime.utcnow().isoformat()
-        db.add("bookmarks", data)
-        return "Bookmark added!"
-
-
-def test_list_bookmarks_command():
-    # Create some test bookmarks
-    bookmarks = [
-        {"url": "https://www.example1.com", "title": "Example 1", "description": "A website for examples"},
-        {"url": "https://www.example2.com", "title": "Example 2", "description": "Another website for examples"},
-        {"url": "https://www.example3.com", "title": "Example 3", "description": "Yet another website for examples"}
-    ]
 
--- test_database.py
+++ test_database.py
@@ -79,3 +79,69 @@ def test_database_manager_add_bookmark(database_manager):
     cursor = conn.cursor()
     cursor.execute(''' SELECT * FROM bookmarks WHERE title='test_title' ''')    
     assert cursor.fetchone()[0] == 1    
+
+    
+def test_database_manager_delete_bookmark(database_manager):
+
+    # arrange
+    database_manager.create_table(
+        "bookmarks",
+        {
+            "id": "integer primary key autoincrement",
+            "title": "text not null",
+            "url": "text not null",
+            "notes": "text",
+            "date_added": "text not null",
+        },
+    )
+
+    criteria = {
+        "id": "integer primary key autoincrement",
+        "title": "test_title",
+        "url": "http://example.com",
+        "notes": "test notes",
+        "date_added": datetime.utcnow().isoformat()        
+    }
+
+    # act
+    database_manager.delete("bookmarks", criteria)
+
+    # assert
+    conn = database_manager.connection
+    cursor = conn.cursor()
+    cursor.execute('SELECT COUNT(*) FROM bookmarks WHERE id=?', (1,))
+    assert cursor.fetchone()[0] == 0    
+    #print(cursor.rowcount)
+
+
+def test_database_manager_select_bookmark(database_manager):
+    # Arrange
+    database_manager.create_table(
+        "bookmarks",
+        {
+            "id": "integer primary key autoincrement",
+            "title": "text not null",
+            "url": "text not null",
+            "notes": "text",
+            "date_added": "text not null",
+        },
+    )
+    bookmark = {
+        "title": "test_title",
+        "url": "http://example.com",
+        "notes": "test notes",
+        "date_added": datetime.utcnow().isoformat(),
+    }
+    database_manager.add("bookmarks", bookmark)
+
+    # Act
+    cursor = database_manager.select(
+        "bookmarks", {"title": "test_title"}, order_by=None
+    )
+    result = cursor.fetchone()
+
+    # Assert
+    if result is not None:
+        assert result[0] == 1, f"Expected 1 row, found {result[0]}"
+    else:
+        assert False, "No rows found with title 'test_title'"
